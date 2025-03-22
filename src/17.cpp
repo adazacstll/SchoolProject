df473ebc4549a81636f43007f2c7e7af58a62693
adazacstll <adazacstll@gmail.com>
2025-03-22 16:47:34 +0000

    Add 17.cpp

---
+++ src/17.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    double y = 2.5;
+    
+    // Print the values of x and y
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+    
+    return 0;
+}
