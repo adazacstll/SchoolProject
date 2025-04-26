96c6b06d6baf12149989dfd7e9442f3a1ec802a8
adazacstll <adazacstll@gmail.com>
2025-04-26 00:13:03 +0000

    Add 34.cpp

---
+++ src/34.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    cout << "The value of x is: " << x << endl;
+
+    if (x > 0) {
+        cout << "x is greater than zero." << endl;
+    } else {
+        cout << "x is not greater than zero." << endl;
+    }
+
+    return 0;
+}
