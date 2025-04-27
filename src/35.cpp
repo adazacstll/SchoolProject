2b4773723ce719493b37fb5691a889e061884128
adazacstll <adazacstll@gmail.com>
2025-04-27 02:06:58 +0000

    Add 35.cpp

---
+++ src/35.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printMessage(const char* message) {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+    // Call a function to demonstrate how to use functions and variables
+    printMessage("This is the first line of code.");
+
+    return 0;
+}
