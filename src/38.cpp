911ed4961e6fa977f586d4785ed9ac1ae61dc0b2
adazacstll <adazacstll@gmail.com>
2025-05-18 13:41:48 +0000

    Add 38.cpp

---
+++ src/38.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height = 2.5;
+    double radius = 10.0; // you can replace this with a variable or input from user
+
+    cout << "Height: " << height << ", Radius: " << radius << endl;
+
+    return 0;
+}
