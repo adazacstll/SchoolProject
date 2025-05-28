eab4c6bf213674fa660348daf1d99705f482423b
adazacstll <adazacstll@gmail.com>
2025-05-28 05:19:16 +0000

    Add 47.cpp

---
+++ src/47.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+    float z = 7.89f;
+
+    std::cout << "x: " << x << ", y: " << y << ", z: " << z << std::endl;
+
+    return 0;
+}
